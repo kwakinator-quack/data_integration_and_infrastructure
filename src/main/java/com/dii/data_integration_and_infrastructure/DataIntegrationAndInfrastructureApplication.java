2cb79842058bc9f902bb6c42f9f151a3f0470537
kwakinator-quack <127004926+kwakinator-quack@users.noreply.github.com>
2025-03-04 00:16:12 -0500

    First Commit

---
+++ src/main/java/com/dii/data_integration_and_infrastructure/DataIntegrationAndInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.dii.data_integration_and_infrastructure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataIntegrationAndInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataIntegrationAndInfrastructureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dii/data_integration_and_infrastructure/DataIntegrationAndInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dii.data_integration_and_infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataIntegrationAndInfrastructureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
